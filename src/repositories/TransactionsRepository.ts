9e4e95f6dd8453b4489ffa12fb0e3a593b599255
lucascprazeres <olucascostadp@gmail.com>
2020-06-22 22:19:15 -0300

    passa nos testes 🚀

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,17 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
+import transactionRouter from '../routes/transaction.routes';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +22,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTotal = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .map(transaction => transaction.value)
+      .reduce((total: number, values: number) => total + values, 0);
+
+    const outcomeTotal = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .map(transaction => transaction.value)
+      .reduce((total: number, values: number) => total + values, 0);
+
+    const balance = incomeTotal - outcomeTotal;
+
+    const balanceObject = {
+      income: incomeTotal,
+      outcome: outcomeTotal,
+      total: balance,
+    };
+
+    return balanceObject;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    };
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
